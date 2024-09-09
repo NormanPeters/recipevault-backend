4607e21d7ecf026083e6474bb3a0fa92df65695f
Barrique-v1 <peters.norman91@gmail.com>
2024-09-09 09:54:47 +0200

    feat: add database connection and hello controller

---
+++ src/main/java/com/recipevault/recipevault/HelloController.java
@@ -0,0 +1,14 @@
+package com.recipevault.recipevault;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Recipe Vault!";
+    }
+
+}
